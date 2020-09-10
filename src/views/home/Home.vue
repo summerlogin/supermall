8c7308414b66f5ee8b2833e855377a61cccac4cb
summerlogin <1169391831@qq.com>
2020-09-10 17:34:20 +0800

    导航栏slot（home）

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,31 @@
 <template>
-  <h2>首页</h2>
+  <div id="Home">
+    <nav-bar class="nav-home">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
 
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      console.log(res)
+    });
+  },
 };
 </script>
 
 <style scoped>
+.nav-home {
+  background: var(--color-tint);
+  color: white;
+}
 </style>
